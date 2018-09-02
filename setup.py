2dbdec36f0d9e4d8d6fcf88debe34b87e467eea7
Idan Haim Shalom <haimdude@gmail.com>
2018-09-02 14:28:22 +0300

    RavenDB-11802 Update version for updating RavenDB server to PyPi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pyravendb-embedded',
     packages=find_packages(),
     long_description=open('README.rst').read(),
-    version='4.1.1.2',
+    version='4.1.1.3',
     description='RavenDB Embedded library to run ravendb in embedded way',
     author='RavenDB',
     author_email='support@ravendb.net',
