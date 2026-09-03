77e76a6bbb50a4059a11a4503e145a45265367d3
Gracjan Sadowicz <gracjan@ravendb.net>
2026-09-03 14:49:02 +0200

    Loosen ravendb client pin to ~=7.2.3 and release 7.2.5.post3
    The exact pin ravendb==7.2.3.post1 conflicts with the published client
    7.2.3.post2: users installing the new client get a dependency mismatch,
    and resolvers like uv downgrade the client back to post1.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     include_package_data=True,
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    version="7.2.5.post2",
+    version="7.2.5.post3",
     description="RavenDB Embedded library to run RavenDB in an embedded way",
     author="RavenDB",
     author_email="support@ravendb.net",
@@ -54,7 +54,7 @@ setup(
     license="Custom EULA",
     keywords="ravendb embedded database nosql doc db",
     install_requires=[
-        "ravendb==7.2.3.post1",
+        "ravendb~=7.2.3",
         "cryptography>=42.0.0",
     ],
     license_files=["LICENSE"],
